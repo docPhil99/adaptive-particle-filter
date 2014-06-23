e40a2b75f841b665da0bc679a0082faf562ca088
Phil Birch <tafj0@eai011110.engg.susx.ac.uk>
2014-06-23 11:29:25 +0100

    Initial Commit

---
+++ WH_tracker/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  WH_tracker
+//
+//  Created by Phil Birch on 23/06/2014.
+//  Copyright (c) 2014 Phil Birch. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
